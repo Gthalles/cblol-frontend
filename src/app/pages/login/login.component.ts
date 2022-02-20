ef5aa46f39502e04af851abb6caf3cd0a3494aa5
Gthalles <garbelottithalles@gmail.com>
2022-02-20 17:26:48 -0300

    Implementa página de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+/* eslint-disable no-empty-function */
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, Form } from '@angular/forms';
+
+import { FormValidationService } from 'src/app/shared/services/form-validation.service';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: [ './login.component.css' ]
 })
-export class LoginComponent
+export class LoginComponent implements OnInit
 {
+    public loginForm!: FormGroup;
 
+    // eslint-disable-next-line no-useless-constructor
+    constructor (private formBuilder: FormBuilder)
+    {}
 
+    ngOnInit (): void
+    {
+        this.loginForm = this.formBuilder.group({
+            email: [
+                null,
+                [
+                    Validators.required,
+                    Validators.maxLength(120),
+                    FormValidationService.validateEmail
+                ]
+            ],
+            password: [
+                null,
+                [
+                    Validators.required,
+                    Validators.minLength(6),
+                    Validators.maxLength(22),
+                    FormValidationService.validatePassword
+                ]
+            ]
+        });
+    }
+
+    static onSubmit (ngForm: Form): Form
+    {
+        return ngForm;
+    }
+
+    getFieldControl (fieldName: string): any
+    {
+        return this.loginForm?.get(fieldName);
+    }
+
+    login (credentials: string[]): void
+    {
+        console.log(credentials);
+        this.loginForm.reset();
+    }
 }
